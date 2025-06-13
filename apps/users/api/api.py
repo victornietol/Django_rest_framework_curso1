ff58a9add734d5477c85ff9ea270b5434b445cfb
victornietol <nietovirgo17@gmail.com>
2025-06-13 16:32:37 -0600

    Manejo de los Response

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -13,16 +13,42 @@ class UserAPIView(APIView):
 
 # Utilizando un decorador
 # Hace lo mismo que la clase anterior para manejar las peticiones, pero aqui se hace con una funcion
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'POST'])
 def user_api_view(request):
     if request.method == "GET": # Si es peticion GET entonces hace lo mismo que la clase de arriba
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data, status=status.HTTP_200_OK)
     
-    elif request.method == "PUT":
+    elif request.method == "POST": #Crear usuario
         user_serializer = UserSerializer(data=request.data) # Convertir al modelo
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST) # Si falla
+
+# Obtener detalles de un objeto,  actualizar un objeto, eliminar un objeto
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail_api_view(request, pk=None):
+    user = User.objects.filter(id=pk).first()
+    if user:
+        if request.method == "GET":
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data)
+
+        elif request.method == "PUT":
+            user_serializer = UserSerializer(user, data=request.data) # (Instancia del usuario a modificar, informacion nueva del usuario)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        elif request.method == "DELETE":
+            user.delete()
+            return Response({"message:""Usuario eliminado."}, status=status.HTTP_200_OK)
+    
+    return Response(
+        {"message":"No se ha encontrado el usuario"}, 
+        status=status.HTTP_400_BAD_REQUEST
+    )
+
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView, user_api_view
+from apps.users.api.api import UserAPIView, user_api_view, user_detail_api_view
 
 urlpatterns = [
     path("usuario/", UserAPIView.as_view(), name="usuario_api"),
     path("usuario_v2/", user_api_view, name="usuario_api"),
+    path("usuario/<int:pk>/", user_detail_api_view, name="usuario_api"),
 ]
